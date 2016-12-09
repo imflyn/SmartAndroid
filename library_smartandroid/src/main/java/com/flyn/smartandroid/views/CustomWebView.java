6ec618c7455c02b03978717b30f42c731f8c3f5e
imflyn <flyn.yu@fandine.com>
2016-12-09 15:42:07 +0800

    Fix error

--- library_smartandroid/src/main/java/com/flyn/smartandroid/views/CustomWebView.java
+++ library_smartandroid/src/main/java/com/flyn/smartandroid/views/CustomWebView.java
@@ -17,36 +17,29 @@ import android.widget.TextView;
 import com.flyn.smartandroid.R;
 import com.flyn.smartandroid.app.Application;
 import com.flyn.smartandroid.util.NetWorkUtil;
-import com.flyn.smartandroid.views.Observable.ObservableScrollViewCallbacks;
-import com.flyn.smartandroid.views.Observable.ObservableWebView;
 
-public class CustomWebView extends FrameLayout
-{
-    private WebViewToolBar    webViewToolBar;
-    private ObservableWebView mWebView;
-    private TextView          tv_reload;
-    private LinearLayout      ll_nonet;
-    private String            mUrl;
-    private WebChromeClient   webChromeClient;
-    private WebViewListener   webViewListener;
+public class CustomWebView extends FrameLayout {
+    private WebViewToolBar webViewToolBar;
+    private WebView mWebView;
+    private TextView tv_reload;
+    private LinearLayout ll_nonet;
+    private String mUrl;
+    private WebChromeClient webChromeClient;
+    private WebViewListener webViewListener;
     private boolean shouldOverrideUrl = false;
 
-    public CustomWebView(Context context)
-    {
+    public CustomWebView(Context context) {
         super(context);
-        if (isInEditMode())
-        {
+        if (isInEditMode()) {
             return;
         }
 
         initView();
     }
 
-    public CustomWebView(Context context, AttributeSet attrs)
-    {
+    public CustomWebView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        if (isInEditMode())
-        {
+        if (isInEditMode()) {
             return;
         }
 
@@ -54,104 +47,84 @@ public class CustomWebView extends FrameLayout
 
     }
 
-    public void setWebViewListener(WebViewListener webViewListener)
-    {
+    public void setWebViewListener(WebViewListener webViewListener) {
         this.webViewListener = webViewListener;
     }
 
-    public ObservableWebView getWebView()
-    {
+    public WebView getWebView() {
         return mWebView;
     }
 
-    public void setWebChromeClient(WebChromeClient webChromeClient)
-    {
+    public void setWebChromeClient(WebChromeClient webChromeClient) {
         this.webChromeClient = webChromeClient;
     }
 
-    private void initView()
-    {
+    private void initView() {
         View view = LayoutInflater.from(getContext()).inflate(R.layout.view_custom_webview, null);
         view.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
         addView(view);
 
-        mWebView = (ObservableWebView) findViewById(R.id.wv_content);
+        mWebView = (WebView) findViewById(R.id.wv_content);
         webViewToolBar = (WebViewToolBar) findViewById(R.id.webview_toolbar);
         webViewToolBar.setWebView(mWebView);
         tv_reload = (TextView) findViewById(R.id.tv_reload);
         ll_nonet = (LinearLayout) findViewById(R.id.ll_nonet);
 
-        tv_reload.setOnClickListener(new OnClickListener()
-        {
+        tv_reload.setOnClickListener(new OnClickListener() {
 
             @Override
-            public void onClick(View v)
-            {
-                if (NetWorkUtil.isNetworkConnected(getContext()))
-                {
+            public void onClick(View v) {
+                if (NetWorkUtil.isNetworkConnected(getContext())) {
                     mWebView.removeAllViews();
                     mWebView.loadUrl("about:blank");
                     loadUrl(mUrl);
                     hiddenErrorWebPage();
-                    Application.getInstance().runOnUiThreadDelay(new Runnable()
-                    {
+                    Application.getInstance().runOnUiThreadDelay(new Runnable() {
                         @Override
-                        public void run()
-                        {
+                        public void run() {
                             mWebView.setVisibility(VISIBLE);
                         }
                     }, 500);
-                } else
-                {
+                } else {
                     showErrorWebPage();
                 }
             }
         });
 
-        mWebView.setWebViewClient(new WebViewClient()
-        {
+        mWebView.setWebViewClient(new WebViewClient() {
 
             @Override
-            public boolean shouldOverrideUrlLoading(WebView view, String url)
-            {
-                if (shouldOverrideUrl)
-                {
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                if (shouldOverrideUrl) {
                     view.loadUrl(url);
                     return true;
-                } else
-                {
+                } else {
                     return false;
                 }
             }
 
             @Override
-            public void onPageStarted(WebView view, String url, Bitmap favicon)
-            {
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
                 super.onPageStarted(view, url, favicon);
-                if (null != webViewListener)
-                {
+                if (null != webViewListener) {
                     webViewListener.onReceivedTitle(view.getTitle());
                 }
             }
 
             @Override
-            public void onPageFinished(WebView view, String url)
-            {
+            public void onPageFinished(WebView view, String url) {
                 super.onPageFinished(view, url);
-                if (null != webViewListener)
-                {
+                if (null != webViewListener) {
                     webViewListener.onReceivedTitle(view.getTitle());
                 }
-                if (null != webViewListener)
-                {
+                if (null != webViewListener) {
                     webViewListener.onPageFinished(view, url);
                 }
                 addImageClickListener();
             }
 
             @Override
-            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl)
-            {
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
                 super.onReceivedError(view, errorCode, description, failingUrl);
                 mWebView.setVisibility(GONE);
                 showErrorWebPage();
@@ -165,28 +138,23 @@ public class CustomWebView extends FrameLayout
         mWebView.setSaveEnabled(false);
         mWebView.addJavascriptInterface(new MyJavascriptInterface(), "imagelistner");
 
-        mWebView.setWebChromeClient(new WebChromeClient()
-        {
+        mWebView.setWebChromeClient(new WebChromeClient() {
             @Override
-            public void onProgressChanged(WebView view, int newProgress)
-            {
+            public void onProgressChanged(WebView view, int newProgress) {
                 super.onProgressChanged(view, newProgress);
 
                 webViewToolBar.setProgressNumber(newProgress);
 
-                if (null != webChromeClient)
-                {
+                if (null != webChromeClient) {
                     webChromeClient.onProgressChanged(view, newProgress);
                 }
 
             }
 
             @Override
-            public void onReceivedTitle(WebView view, String title)
-            {
+            public void onReceivedTitle(WebView view, String title) {
                 super.onReceivedTitle(view, title);
-                if (null != webViewListener)
-                {
+                if (null != webViewListener) {
                     webViewListener.onReceivedTitle(title);
                 }
             }
@@ -195,13 +163,11 @@ public class CustomWebView extends FrameLayout
 
     }
 
-    public void setShouldOverrideUrl(boolean shouldOverrideUrl)
-    {
+    public void setShouldOverrideUrl(boolean shouldOverrideUrl) {
         this.shouldOverrideUrl = shouldOverrideUrl;
     }
 
-    private void addImageClickListener()
-    {
+    private void addImageClickListener() {
         this.mWebView.loadUrl("javascript:(function(){" + " var objs = document.getElementsByTagName(\"img\"); " + " var arr=new Array(); for   " +
                 "(var i=0;i <objs.length;i++) { arr[i]= objs[i].src;}  " + "for(var i=0;i<objs.length;i++)  " + "{" + "    objs[i].onclick=function" +
                 "()  " + "    {  " + "        window.imagelistner.openImage(arr,this.src);  " + "    }  " + "}" + "})()");
@@ -209,88 +175,66 @@ public class CustomWebView extends FrameLayout
     }
 
 
-    public void loadUrl(String url)
-    {
+    public void loadUrl(String url) {
         this.mUrl = url;
         this.mWebView.loadUrl(url);
     }
 
-    public void onResume()
-    {
-        if (Build.VERSION.SDK_INT >= 11)
-        {
+    public void onResume() {
+        if (Build.VERSION.SDK_INT >= 11) {
             mWebView.onResume();
-        } else
-        {
-            try
-            {
+        } else {
+            try {
                 mWebView.getClass().getMethod("onResume").invoke(mWebView, (Object[]) null);
-            } catch (Exception e)
-            {
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
     }
 
-    public void onPause()
-    {
+    public void onPause() {
 
-        if (Build.VERSION.SDK_INT >= 11)
-        {
+        if (Build.VERSION.SDK_INT >= 11) {
             mWebView.onPause();
-        } else
-        {
-            try
-            {
+        } else {
+            try {
                 mWebView.getClass().getMethod("onPause").invoke(mWebView, (Object[]) null);
-            } catch (Exception e)
-            {
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
 
     }
 
-    public void destroy()
-    {
+    public void destroy() {
         mWebView.stopLoading();
         mWebView.loadUrl("about:blank");
         mWebView.removeAllViews();
         removeAllViews();
     }
 
-    public void showErrorWebPage()
-    {
+    public void showErrorWebPage() {
         ll_nonet.setVisibility(View.VISIBLE);
     }
 
-    public void hiddenErrorWebPage()
-    {
+    public void hiddenErrorWebPage() {
         ll_nonet.setVisibility(View.GONE);
     }
 
-    public void hiddenWebViewToolBar()
-    {
+    public void hiddenWebViewToolBar() {
         this.webViewToolBar.setVisibility(View.GONE);
     }
 
-    public void showWebViewToolBar()
-    {
+    public void showWebViewToolBar() {
         this.webViewToolBar.setVisibility(View.VISIBLE);
     }
 
-    public WebViewToolBar getWebViewToolBar()
-    {
+    public WebViewToolBar getWebViewToolBar() {
         return this.webViewToolBar;
     }
 
-    public void setScrollViewCallbacks(ObservableScrollViewCallbacks scrollViewCallbacks)
-    {
-        mWebView.setScrollViewCallbacks(scrollViewCallbacks);
-    }
 
-    public interface WebViewListener
-    {
+    public interface WebViewListener {
         void onReceivedTitle(String title);
 
         void onOpenImage(String[] urlList, int position);
@@ -298,19 +242,14 @@ public class CustomWebView extends FrameLayout
         void onPageFinished(WebView view, String url);
     }
 
-    private class MyJavascriptInterface
-    {
+    private class MyJavascriptInterface {
 
         @JavascriptInterface
-        public void openImage(String[] urlList, String url)
-        {
-            if (null != webViewListener)
-            {
+        public void openImage(String[] urlList, String url) {
+            if (null != webViewListener) {
 
-                for (int i = 0; i < urlList.length; i++)
-                {
-                    if (url.equals(urlList[i]))
-                    {
+                for (int i = 0; i < urlList.length; i++) {
+                    if (url.equals(urlList[i])) {
                         webViewListener.onOpenImage(urlList, i);
                         break;
                     }
